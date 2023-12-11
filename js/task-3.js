7554e941353223e74f26e6bcea0b380ff57fc9e5
ValeriiaVyshniak <lerchik.vishnyak@gmail.com>
2023-12-11 18:00:17 +0100

    js-hw-01-ver-01

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderThickness = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderThickness
+    return totalWidth
+}
+
+    
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
